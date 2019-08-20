c6f7c1ea3419f48609f9f71c8641ae89538beb6d
carlos alcala <solenasthellion@gmail.com>
2019-08-20 11:37:17 -0500

    :sparkles: removed demo renders, added new comps

--- src/AppWithHooks.jsx
+++ src/AppWithHooks.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
-import Demo from "./containers/Demo.jsx";
 //font awesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faDiceD20 } from "@fortawesome/free-solid-svg-icons";
+import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
-library.add(fab, faDiceD20);
+library.add(fab, faArrowAltCircleDown);
 
 function AppWithHooks() {
-  return <Demo />;
+  return (
+    <div>
+      <Hero />
+      <About />
+    </div>
+  );
 }
 
 render(<AppWithHooks />, document.getElementById("root"));
