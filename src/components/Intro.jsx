7ed36cd5a9dfc7774f780cd3ebd4b24060d7f500
carlos alcala <solenasthellion@gmail.com>
2019-08-20 11:37:30 -0500

    :sparkles: test component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Intro(props) {
+  // const { greeting, rules } = props;
+  return (
+    <div>
+      <h1>Three buckets</h1>
+      <h2>RUELS: </h2>
+      <h3>DO THING</h3>
+    </div>
+  );
+}
+
+export default Intro;
