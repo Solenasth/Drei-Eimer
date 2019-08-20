807cf8c95590e8acd14f3e296c9c2886e3fb6fac
carlos alcala <solenasthellion@gmail.com>
2019-08-20 11:36:41 -0500

    :sparkles: Hero image with animation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const Gradient = `
+45deg, rgba(131,58,180,.8) 0%, rgba(253,29,29,.8) 50%, rgba(252,176,69,.8) 100%
+`;
+
+const HeroDiv = styled.div`
+  height: 50vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(${Gradient});
+  background-size: 600% 600%;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 2em;
+  border-width: 0.1em;
+  border-style: solid;
+  padding: 1em;
+  background-color: rgba(255, 255, 255, 0.1);
+`;
+
+function Hero() {
+  return (
+    <HeroDiv className="Hero">
+      <Title> Drei Einer Challange </Title>
+    </HeroDiv>
+  );
+}
+export default Hero;
