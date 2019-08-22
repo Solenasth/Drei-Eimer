07bb27f6fe53144f4c08b4a72aaa6f0eeb075cdf
carlos alcala <solenasthellion@gmail.com>
2019-08-22 02:13:40 -0500

    :bento: adding asset icons and barrel export

---
+++ src/database/resources.js
@@ -0,0 +1,15 @@
+import empty from "../resources/empty.png";
+import full from "../resources/full.png";
+import semi from "../resources/semi.png";
+import tap from "../resources/tap.png";
+import trash from "../resources/trash.png";
+
+const resources = {
+  icon_empty: empty,
+  icon_full: full,
+  icon_semi: semi,
+  icon_tap: tap,
+  icon_trash: trash
+};
+
+export default resources;
