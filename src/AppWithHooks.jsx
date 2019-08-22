6ad77d4bfedae3ff687ba95c1a166c9120bb39d0
carlos alcala <solenasthellion@gmail.com>
2019-08-22 02:14:41 -0500

    :construction: test print

--- src/AppWithHooks.jsx
+++ src/AppWithHooks.jsx
@@ -3,19 +3,21 @@ import { render } from "react-dom";
 //font awesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Hero from "./components/Hero.jsx";
 import About from "./containers/About.jsx";
 import Step from "./components/Step.jsx";
 import Solution from "./components/Solution.jsx";
+import { SOLUTION1 } from "./database/premade";
 
-library.add(fab, faArrowAltCircleDown);
+library.add(fab, faArrowRight);
 
 function AppWithHooks() {
   return (
     <div>
       <Hero />
       <About />
+      <Solution steps={SOLUTION1.steps} finished={SOLUTION1.finished} />
       <Solution />
     </div>
   );
